8bfffd60fb57e9e5c99ddd8587eaf47c63553719
rodogi <rodgdor@gmail.com>
2018-03-20 02:14:16 +0100

    First commit

---
+++ rmsd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Calculate the rooted mean squared distance between two protein structures.
+
+"""
+__author__ = "Rodrigo Dorantes-Gilardi"
+__email__ = "rodgdor@gmail.com"
+
+import Bio.PDB
